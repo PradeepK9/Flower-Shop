89600ba3792e3a8ef36c0a3fbdc74fca8d6552b0
Pradeep Jaiswal <pradeep88318@mastek.com>
2023-09-01 00:40:18 +0530

    auth routes added

--- flowershop-api/index.js
+++ flowershop-api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+
+const authRouter = require("./routes/auth");
 
 const port = 3002;
 
@@ -11,6 +14,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
     console.log(`Flower Shop app listening at http://localhost:${port}`);
