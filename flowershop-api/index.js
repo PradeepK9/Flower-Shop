0dfa2cf648437198eeb3b0795cb0b593c64bafb7
Pradeep Jaiswal <pradeep88318@mastek.com>
2023-09-01 00:28:36 +0530

    loggers and cors added

--- flowershop-api/index.js
+++ flowershop-api/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
 const port = 3002;
 
 const app = express();
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.listen(port, () => {
     console.log(`Flower Shop app listening at http://localhost:${port}`);
