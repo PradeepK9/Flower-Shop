1ae4f2bf096d40af5fb86dd048cd67b3cbb15222
Pradeep Jaiswal <pradeep88318@mastek.com>
2023-09-01 21:16:55 +0530

    error handler

--- flowershop-api/index.js
+++ flowershop-api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const createError = require("http-errors");
 
 const authRouter = require("./routes/auth");
 
@@ -16,6 +17,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/auth", authRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    console.log(err);
+    res.status(err.status || 500);
+});
 
 app.listen(port, () => {
     console.log(`Flower Shop app listening at http://localhost:${port}`);
